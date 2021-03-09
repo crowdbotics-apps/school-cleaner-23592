c09174d193e6f2d2ae6cf8a28aa4a6feee78d89a
Hashir <37709306+mhashirhassan22@users.noreply.github.com>
2021-03-09 16:47:21 +0500

    Update urls.py

--- school_cleaner_23592/urls.py
+++ school_cleaner_23592/urls.py
@@ -21,6 +21,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
 urlpatterns = [
     path("", include("home.urls")),
     path("accounts/", include("allauth.urls")),
@@ -28,10 +29,11 @@ urlpatterns = [
     path("api/v1/", include("home.api.v1.urls")),
     path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
-    path("rest-auth/", include("rest_auth.urls")),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+    
     # Override email confirm to use allauth's HTML view instead of rest_auth's API view
     path("rest-auth/registration/account-confirm-email/<str:key>/", confirm_email),
-    path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
 admin.site.site_header = "School cleaner"
