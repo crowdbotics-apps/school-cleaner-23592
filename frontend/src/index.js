7866e32b037941d11416e375996debcc0f544920
Hassan Mahmood <devhassan.mahmood@gmail.com>
2021-03-17 17:39:28 +0500

    session maintainance

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,42 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './main/App';
+// import reportWebVitals from './reportWebVitals';
+// import { Provider } from 'react-redux';
+// import configureStore from './store';
+
+// ReactDOM.render(
+//   <Provider store={configureStore()}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './main/App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import configureStore from './store';
+import { ThemeProvider } from 'styled-components';
+import { ConnectedRouter } from 'connected-react-router';
+
+import configureStore, { history } from './store';
+import App from './main/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const initialState = {};
+
+const store = configureStore(initialState);
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div>
+          <App />
+        </div>
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root'),
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
--- frontend/src/main/App.js
+++ frontend/src/main/App.js
@@ -1,12 +1,71 @@
-import './App.scss';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Routes from './routes'
+// import './App.scss';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import Routes from './routes'
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes/>
+//     </BrowserRouter>
+//   );
+// }
+
+
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+
+import './App.scss';
+import routes from './routes';
+import { isLoggedIn } from '../utils/utility';
+// import { getUserData } from '../modules/actions/UserActions';
+
+const renderRoutes = () => {
+  const renderRoute = (routerProps, Component, props, isPrivate = false) => {
+    if (Component) {
+      const componentProps = {
+        ...routerProps,
+        ...props,
+      };
+      if (isPrivate) {
+        return isLoggedIn() ? (
+          <Component {...componentProps} />
+        ) : (
+          <Redirect to="/login" />
+        );
+      }
+      return isLoggedIn() ? (
+        <Redirect to="/dashboard" />
+      ) : (
+        <Component {...componentProps} />
+      ); // eslint-disable-line
+    }
+    return null;
+  };
+
+  return routes.map((route) => (
+    <Route
+      key={route.name}
+      exact={route.exact}
+      path={route.path}
+      render={(routerProps) =>
+        renderRoute(routerProps, route.component, route.props, route.isPrivate)
+      }
+    />
+  ));
+};
+
+const Router = () => <Switch>{renderRoutes()}</Switch>;
+
+
+const App = () => {
+  
   return (
-    <BrowserRouter>
-      <Routes/>
-    </BrowserRouter>
+    <div>
+      <Router />
+    </div>
   );
-}
+};
+
+export default withRouter(App);
 
