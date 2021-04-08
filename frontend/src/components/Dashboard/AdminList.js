c811bed585d17d62a129080662cd72309d0453a8
Jahanzaib Akhter <jahanzaib.akhter@crowdbotics.com>
2021-04-08 23:30:34 +0500

    UPDATE: Build and some fixes.

--- frontend/src/components/Dashboard/AdminList.js
+++ frontend/src/components/Dashboard/AdminList.js
@@ -1,7 +1,7 @@
 import React,{ useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../../modules/actions/UserActions';
-import { fetchAdmins } from '../../modules/actions/AdminActions';
+import { fetchAdmins, getDistricEmployees } from '../../modules/actions/AdminActions';
 import { updateDistrict, fetchDistricts } from '../../modules/actions/DistrictActions';
 
 const AdminList = (props) => {
@@ -10,17 +10,28 @@ const AdminList = (props) => {
   const { admins: { loading, success, error, data }} = useSelector(({ admin }) => admin);
 
   const [users, setUsers] = useState([]);
+
   const [selectedAdmins, setselectedAdmins] = useState([]);
+  const [checkedValue, setCheckedValue] = useState(false);
+
+  console.log('selectedAdmins', selectedAdmins);
+
+  const handleChecked = () => {
+    setCheckedValue(!checkedValue);
+  }
+
+  console.log('roleId', data);
 
   useEffect(() => {
-    dispatch(fetchAdmins());
+    // dispatch(fetchAdmins());
+
     dispatch(fetchDistricts());
   }, []);
 
 
   const handleCheckboxChange = (event) => {
     let admins = selectedAdmins
-    let adminId = event.target.value
+    let adminId = event.target.checkedValue
 
     if(admins.includes(adminId)) {
       let index = admins.indexOf(adminId)
@@ -46,14 +57,14 @@ const AdminList = (props) => {
           <div className="modal-content">
             <div className="modal-body">
               <div className="admin-info m-4">
-                {data.length > 0 && data.map(admin => {
-                  return (
-                    <div className="d-flex items-center justify-content-between mb-4">
-                      <div>{admin.name}</div>
-                      <div><input type="checkbox" className="form-check-input big-checkbox" value={admin.id} onChange={handleCheckboxChange}/></div>
+                {data.length > 0 && data.map(admin =>
+                  <div className="d-flex items-center justify-content-between mb-4">
+                    <div>{admin.employee_detail.name}</div>
+                    <div>
+                      <input type="checkbox" className="form-check-input big-checkbox" value={admin.employee_detail.id} onChange={handleCheckboxChange}/>
                     </div>
-                  ); 
-                })}
+                  </div>
+                )}
               </div>
               <div class="d-flex justify-content-center align-items-center btn-holder">
                 <button type="button" class="btn btn-primary text-uppercase" data-bs-toggle="modal" data-bs-target="#add_admin" onClick={saveHandler}>Save</button>
